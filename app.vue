564db408ed8cf6cca00b75602465a920c81de147
Chris Janssen <chris_janssen@Chriss-iMac.local>
2020-02-20 21:21:47 -0800

    added VUE

---
+++ app.vue
@@ -0,0 +1,17 @@
+// App.vue
+<template>
+  <div id="app">
+    <DataDisplay/>
+  </div>
+</template>
+
+<script>
+import DataDisplay from './datadisplay.vue'
+
+export default {
+  name: "App",
+  components: {
+    DataDisplay
+  }
+};
+</script>
